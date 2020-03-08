53367d4b7135c90c9f2071f6467829538f548530
Максим Мерешенский <meresh97@yandex.ru>
2020-03-08 20:25:09 +0200

    Добавьте файлы проекта.

---
+++ ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using DemoLibrary;
+using DemoLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+	public static class ContainerConfig
+	{
+		public static IContainer Configure()
+		{
+			var builder = new ContainerBuilder();
+
+			//builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+			builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
+			builder.RegisterType<Logger>().As<ILogger>();
+			builder.RegisterType<DataAccess>().As<IDataAccess>();
+
+			return builder.Build();
+		}
+	}
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary.Utilities;
+using Autofac;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IContainer container = ContainerConfig.Configure();
+
+            using (ILifetimeScope scope = container.BeginLifetimeScope())
+            {
+                IBusinessLogic businessLogic = scope.Resolve<IBusinessLogic>();
+                businessLogic.ProcessData();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
